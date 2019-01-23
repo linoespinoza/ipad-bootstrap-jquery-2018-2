a383814644e41728ec4a8c76d736540844e6a8dc
Lino Espinoza <lino.espinoza@entel.pe>
2019-01-22 23:24:52 -0500

    Se agregan los archivos de la semana 7 (Ajax, Ajax Validation Forms)

--- js/main.js
+++ js/main.js
@@ -1,114 +1,131 @@
-// jQuery
-$(document).ready(function(){
-
-	jQuery.fx.speeds.fast = 200;
-	jQuery.fx.speeds.blazing = 100;
-	jQuery.fx.speeds.slowmotion = 60000;
-
-	$.each([ 52, 97 ], function( index, value ) {
-  		console.log( index + ": " + value );
-	});
-
-	// Método click jQuery
-	//$('#seeSpeakers').click(function(){
-	//	console.log("Elemento clickeado");
-	//})
-
-	//$('#seeSpeakers').on("click", function(){
-	//	console.log("Elemento clickeado a través del método on");
-	//})
-
-	//$('#seeSpeakers').one("click", function(){
-	//	console.log("Elemento clickeado la primera vez");
-	//})
-
-	$('#seeSpeakers').click(function(event){
-		event.preventDefault();
-		//console.log("Elementos clickeado a través de un bind");
-	})
-
-	$('#seeSpeakers').bind("click", function(event){
-		console.log('Event type', event.type);
-		console.log('pagex', event.pageX);
-		console.log('pagey type', event.pageY);
-		console.log('Target', event.target.innerHTML);
-
-	})
-
-	/* $('#seeSpeakers').hover(
-		function () {
-        	$(this).css({"background-color":"red"});
-        }, 
-        function () {   
-        	$(this).css({"background-color":"blue"});
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
         }
-  	);*/
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
 
-	$('#hide').click(function(){
-		$("#speakers").animate({ 
-	      width: "100",
-	      opacity: 1.0,
-	      marginLeft: "0in",
-	      fontSize: "100%", 
-	      borderWidth: "1px"
-        }, 1500 );
-		//$('#speakers').slideUp("slow", function(){});
-	});
-
-	$('#show').click(function(){
-        $("#speakers").animate({ 
-	      width: "70%",
-	      opacity: 0.4,
-	      marginLeft: "0.6in",
-	      fontSize: "3em", 
-	      borderWidth: "10px"
-        }, 1500 );
-	});
-
-	$('#toogleSpeakers').click(function(){
-		$('#speakers').toggle('slowmotion', function(){
-		
-		});
-	})
-
-	$(document).click(function(){
-		//$('div#about img').toggle("bounce", { times: 3 }, "slow");
-		$('div#about img').toggle("clip");
-	})
-
-	var options = {
-		interval: 2000, 
-		ride: "carousel", 
-		pause: "false"
-	}
-
-	$('.carousel').carousel(options);
-	/* $('#carouselUp').click(function(){
-		var options = {
-			interval: 2000
-		}
-		
-		console.log('Clicked');
-	})*/
-		
-	
-}); // Fin 
+  // jQuery code
+  //console.log('Hola');
+  $.get( "data.html", function( response ) {
+    console.log( response ); // server response
+  });
 
 
+  jQuery.validator.addMethod("isValidRuc", function(v) {
+    ruc = $.trim(v);
+      if(ruc.length == 11) {
+        suma = 0;
+        x = 6;
+        size = ruc.length;
+        for( var i = 0; i < ( size - 1); i++ ){
+          if ( i == 4 ) x = 8;
+          digit = ruc.charAt(i);
+          x--;
 
-// Extracted from: https://api.jquery.com/jQuery.each/
-var arr = [ "one", "two", "three", "four", "five" ];
+          if ( i==0 ) suma += ( digit * x );
+          else suma += ( digit * x );
+        }
+      
+        resto = suma % 11;
+        resto = 11 - resto;
+        
+        if ( resto >= 10) resto = resto - 10;
+        if ( resto == ruc.charAt(size -1)){
+          return true;
+        } 
+      } else {
+        return false;
+      } 
+  }, "Ruc incorrecto, trate de nuevo.");
 
-// objeto javascript
-var obj = { one: 1, two: 2, three: 3, four: 4, five: 5 };
+  // Validate a phone number field
+  /*$("#formRegister").submit(function(event) {
+    var inputtedPhoneNumber = $("#card-number").val();
+
+    var phoneNumberRegex = /^\d*$/;
  
-jQuery.each( arr, function( i, val ) {
-  $( "#" + val ).text( "Mine is " + val + "." );
- 
-  // Will stop running after "three"
-  return ( val !== "three" );
-});
- 
-jQuery.each( obj, function( i, val ) {
-  $( "#" + i ).append( document.createTextNode( " - " + val ) );
-});
+    if ( !phoneNumberRegex.test( inputtedPhoneNumber ) ) {
+        event.preventDefault();
+        var formSerialized = $(this).serialize();
+        console.log(formSerialized);
+
+    } else {
+        // Run $.ajax() here
+        $.ajax({
+          method: $(this).attr('action'),
+          url: "data.html",
+          data: formSerialized 
+        }).done(function(message){
+          console.log(message);
+        });
+    }
+  });*/
+
+  $('#formRegister').validate({
+    rules: {
+      cardNumber : { number: true, minlength: 16, isValidRuc: true },
+      zipCode : { number: true },
+      cvv : { number: true }
+    },
+    messages: {
+      cardNumber : { 
+        required: 'The card number is required',
+        number: 'This must be a number',
+        minlength: 'This should container 16 numbers' },
+      zipCode : { 
+        required: 'The zip code is required',
+        number: 'This must be a number' },
+      cvv : { 
+        required: 'The cvv number is required',
+        number: 'This must be a number' }
+    },
+    errorClass: "error",
+    validClass: "valid-feedback",
+    errorElement: "div",
+    submitHandler: function(form){
+      var options = {
+        target: '',
+        beforeSubmit: function(){ console.log('submitted'); },
+        success: function() { 
+          console.log('success');
+          $('#registerForm').modal('hide');
+          //@todo: crear una alerta que muestre que el formulario se ha enviado correctamente
+        }
+      };
+
+      $(form).ajaxSubmit(options); 
+    }
+  });
+
+  /*$('#formRegister').submit(function() { 
+    
+      return false; 
+  });*/ 
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://pokeapi.co/api/v2/pokemon/mew/",
+    "method": "GET",
+    "headers": {}
+  }
+
+  $.ajax(settings).done(function(response) {
+    var imgUrl = response.sprites.front_default;
+    $('#pokemon').attr('src', imgUrl);
+    $('#pokemonName').text(response.name);
+  })
+
+})();
